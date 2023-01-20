3ad1c1a82805f36ad47c3ba71cbdc1d6e047e95c
tao <wangtaolab@tom.com>
2023-01-20 16:51:55 +0800

    day8

--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -16,11 +16,10 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
+use List::{Nil, Cons};
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -33,11 +32,13 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    // todo!()
+    Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    // todo!()
+    Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))))
 }
 
 #[cfg(test)]
--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,7 +1,7 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -13,6 +13,16 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    let mut m = vec![];
+    for i in 1..=num {
+        m.push(i);
+    }
+
+    m.iter().fold(1, |accum, item| {
+        accum * item
+    })
+
+
 }
 
 #[cfg(test)]
