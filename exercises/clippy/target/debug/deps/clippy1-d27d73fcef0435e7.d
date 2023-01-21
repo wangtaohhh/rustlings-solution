d3cfdba404dca7632b26e73cd8c0cf13bb3aec87
tao <wangtaolab@tom.com>
2023-01-21 18:22:57 +0800

    day9 second part

--- exercises/clippy/target/debug/deps/clippy1-d27d73fcef0435e7.d
+++
@@ -1,7 +0,0 @@
-/home/tao/Desktop/rustlings-solution/exercises/clippy/target/debug/deps/clippy1-d27d73fcef0435e7.rmeta: clippy1.rs
-
-/home/tao/Desktop/rustlings-solution/exercises/clippy/target/debug/deps/clippy1-d27d73fcef0435e7.d: clippy1.rs
-
-clippy1.rs:
-
-# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
---
+++ exercises/clippy/target/debug/deps/clippy3-95b5cd398c05f977.d
@@ -0,0 +1,7 @@
+/home/tao/Desktop/rustlings-solution/exercises/clippy/target/debug/deps/clippy3-95b5cd398c05f977.rmeta: clippy3.rs
+
+/home/tao/Desktop/rustlings-solution/exercises/clippy/target/debug/deps/clippy3-95b5cd398c05f977.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
